a3f8e9dbf3cf17d80ab7871a9f1c12b59fd81d0c
Hackbright Student <info@hackbrightacademy.com>
2014-02-11 11:26:57 -0800

    Exercise 5 complete.

---
+++ lettercount.py
@@ -0,0 +1,20 @@
+from sys import argv
+script,filename = argv
+
+f = open(filename)
+filetext = f.read()
+filetext = filetext.lower()
+letters = list(filetext)
+
+alphabet = [0] * 26
+
+for letter in letters:
+    index = ord(letter) - 97
+    if index > 25 or index < 0:
+        continue
+    else: 
+        alphabet[index] += 1
+
+print alphabet
+
+f.close()
